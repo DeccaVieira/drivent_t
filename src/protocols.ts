eb4bf938a4b5ed9d6e554ab204f31d7addc4dc24
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-20 23:56:30 -0300

    Cep Validation

---
+++ src/protocols.ts
@@ -0,0 +1,39 @@
+export type ApplicationError = {
+  name: string;
+  message: string;
+};
+
+export type ViaCEPAddress = {
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+};
+
+export type RequestError = {
+  status: number;
+  data: object | null;
+  statusText: string;
+  name: string;
+  message: string;
+};
+
+
+export type Data= {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade:string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd:string;
+  siafi: string;
+  erro?:boolean;
+};
+
+export type ApiResponse = {
+  data:Data
+}
